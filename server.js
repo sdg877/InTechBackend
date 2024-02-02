39ba8fcd015cbd092eb4f0c324881bf221ef3adb
tamistyping <enquiretam@gmail.com>
2024-02-02 11:03:55 +0000

    server file creation

---
+++ server.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+import jobRoutes from "./routes/jobs.js";
+import userRoutes from "./routes/users.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.DBURL);
+
+app.get("/", (req, res) => {
+  console.log("inTech");
+});
+
+app.listen(port, () => {
+  console.log(`Server Listening at http://localhost:${port}`);
+});
